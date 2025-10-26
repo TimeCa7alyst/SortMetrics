7526c4c594930944d46fb2d9a92832c7234dd9b2
Denis Alves <denis_alves112@hotmail.com>
2025-10-26 16:13:25 -0300

    netbeans first

--- src/view/QuantityInputVIew.java
+++ src/view/QuantityInputVIew.java
@@ -1,15 +1,13 @@
 package view;
 
+import java.awt.*;
 import javax.swing.*;
 
-public class QuantityInputVIew {
+public class QuantityInputVIew extends JPanel {
     private JPanel mainPanel;
-    private JTextField textField1;
-    private JLabel digiteQuantidadeLabel;
-    private JButton analisarButton;
-    private JButton sairButton2;
 
     public QuantityInputVIew() {
+        initComponents();
         String digiteQuantidade = "Digite a quantidade de números<br>" +
                 "para serem ordenados";
         digiteQuantidadeLabel.setText("<html>" + digiteQuantidade + "</html>");
@@ -35,4 +33,83 @@ public class QuantityInputVIew {
     public JButton getSairButton2() {
         return sairButton2;
     }
+
+    private void initComponents() {
+	// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Denis
+    digiteQuantidadeLabel = new JLabel();
+    textField1 = new JTextField();
+    analisarButton = new JButton();
+    sairButton2 = new JButton();
+
+    //======== this ========
+    setBorder(new javax.swing.border.CompoundBorder(new javax.swing.border.TitledBorder(new javax.
+    swing.border.EmptyBorder(0,0,0,0), "JF\u006frmDesi\u0067ner Ev\u0061luatio\u006e",javax.swing.border
+    .TitledBorder.CENTER,javax.swing.border.TitledBorder.BOTTOM,new java.awt.Font("Dialo\u0067"
+    ,java.awt.Font.BOLD,12),java.awt.Color.red), getBorder
+    ())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){@Override public void propertyChange(java
+    .beans.PropertyChangeEvent e){if("borde\u0072".equals(e.getPropertyName()))throw new RuntimeException
+    ();}});
+
+    //---- digiteQuantidadeLabel ----
+    digiteQuantidadeLabel.setOpaque(false);
+
+    //---- analisarButton ----
+    analisarButton.setAutoscrolls(false);
+    analisarButton.setBackground(Color.white);
+    analisarButton.setBorderPainted(true);
+    analisarButton.setContentAreaFilled(true);
+    analisarButton.setDoubleBuffered(true);
+    analisarButton.setName("");
+    analisarButton.setSelected(false);
+    analisarButton.setText("Analisar");
+    analisarButton.setVerticalAlignment(SwingConstants.CENTER);
+
+    //---- sairButton2 ----
+    sairButton2.setAutoscrolls(false);
+    sairButton2.setBackground(Color.white);
+    sairButton2.setBorderPainted(true);
+    sairButton2.setContentAreaFilled(true);
+    sairButton2.setDoubleBuffered(true);
+    sairButton2.setName("");
+    sairButton2.setSelected(false);
+    sairButton2.setText("Sair");
+    sairButton2.setVerticalAlignment(SwingConstants.CENTER);
+
+    GroupLayout layout = new GroupLayout(this);
+    setLayout(layout);
+    layout.setHorizontalGroup(
+        layout.createParallelGroup()
+            .addGroup(layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 521, GroupLayout.PREFERRED_SIZE))
+            .addComponent(digiteQuantidadeLabel, GroupLayout.PREFERRED_SIZE, 2, GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(110, 110, 110)
+                .addComponent(analisarButton)
+                .addGap(180, 180, 180)
+                .addComponent(sairButton2))
+    );
+    layout.setVerticalGroup(
+        layout.createParallelGroup()
+            .addGroup(layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(textField1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addGap(35, 35, 35)
+                .addComponent(digiteQuantidadeLabel, GroupLayout.PREFERRED_SIZE, 2, GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32)
+                .addGroup(layout.createParallelGroup()
+                    .addComponent(analisarButton)
+                    .addComponent(sairButton2)))
+    );
+	// JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Denis
+    private JLabel digiteQuantidadeLabel;
+    private JTextField textField1;
+    private JButton analisarButton;
+    private JButton sairButton2;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
--- src/view/SelectView.java
+++ src/view/SelectView.java
@@ -2,13 +2,10 @@ package view;
 
 import javax.swing.*;
 import java.awt.*;
+import javax.swing.border.*;
 
-public class SelectView {
+public class SelectView extends JPanel {
     private JPanel mainPanel;
-    private JButton continuarButton;
-    private JComboBox dropDown1;
-    private JLabel label1;
-    private JButton sairButton;
 
 
     Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -16,6 +13,7 @@ public class SelectView {
     int height = (int) (screenSize.height * 0.2);
 
     public SelectView() {
+        initComponents();
         dropDown1.addItem("Bubble Sort");
         dropDown1.addItem("Selection Sort");
         dropDown1.addItem("Insertion Sort");
@@ -47,4 +45,88 @@ public class SelectView {
     public int getWidth() {
         return width;
     }
+
+    private void initComponents() {
+	// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Denis
+    continuarButton = new JButton();
+    sairButton = new JButton();
+    label1 = new JLabel();
+    dropDown1 = new JComboBox();
+
+    //======== this ========
+    setVisible(true);
+    setBorder(new TitledBorder(new EtchedBorder(), ""));
+    setBorder ( new javax . swing. border .CompoundBorder ( new javax . swing. border .TitledBorder ( new
+    javax . swing. border .EmptyBorder ( 0, 0 ,0 , 0) ,  "JF\u006frmDes\u0069gner \u0045valua\u0074ion" , javax
+    . swing .border . TitledBorder. CENTER ,javax . swing. border .TitledBorder . BOTTOM, new java
+    . awt .Font ( "D\u0069alog", java .awt . Font. BOLD ,12 ) ,java . awt
+    . Color .red ) , getBorder () ) );  addPropertyChangeListener( new java. beans .
+    PropertyChangeListener ( ){ @Override public void propertyChange (java . beans. PropertyChangeEvent e) { if( "\u0062order" .
+    equals ( e. getPropertyName () ) )throw new RuntimeException( ) ;} } );
+
+    //---- continuarButton ----
+    continuarButton.setAutoscrolls(false);
+    continuarButton.setBackground(Color.white);
+    continuarButton.setBorderPainted(true);
+    continuarButton.setContentAreaFilled(true);
+    continuarButton.setDoubleBuffered(true);
+    continuarButton.setName("");
+    continuarButton.setSelected(false);
+    continuarButton.setText("Continuar");
+    continuarButton.setVerticalAlignment(SwingConstants.CENTER);
+
+    //---- sairButton ----
+    sairButton.setAutoscrolls(false);
+    sairButton.setBackground(Color.white);
+    sairButton.setBorderPainted(true);
+    sairButton.setContentAreaFilled(true);
+    sairButton.setDoubleBuffered(true);
+    sairButton.setName("");
+    sairButton.setSelected(false);
+    sairButton.setText("Sair");
+    sairButton.setVerticalAlignment(SwingConstants.CENTER);
+
+    //---- label1 ----
+    label1.setText("Escolha um algoritmo de ordena\u00e7\u00e3o");
+
+    GroupLayout layout = new GroupLayout(this);
+    setLayout(layout);
+    layout.setHorizontalGroup(
+        layout.createParallelGroup()
+            .addGroup(layout.createSequentialGroup()
+                .addGap(93, 93, 93)
+                .addComponent(label1)
+                .addGap(114, 114, 114)
+                .addComponent(dropDown1, GroupLayout.PREFERRED_SIZE, 279, GroupLayout.PREFERRED_SIZE))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(167, 167, 167)
+                .addComponent(continuarButton)
+                .addGap(268, 268, 268)
+                .addComponent(sairButton))
+    );
+    layout.setVerticalGroup(
+        layout.createParallelGroup()
+            .addGroup(layout.createSequentialGroup()
+                .addGap(41, 41, 41)
+                .addGroup(layout.createParallelGroup()
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(8, 8, 8)
+                        .addComponent(label1))
+                    .addComponent(dropDown1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addGap(82, 82, 82)
+                .addGroup(layout.createParallelGroup()
+                    .addComponent(continuarButton)
+                    .addComponent(sairButton)))
+    );
+	// JFormDesigner - End of component initialization  //GEN-END:initComponents  @formatter:on
+    }
+
+    // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables  @formatter:off
+    // Generated using JFormDesigner Evaluation license - Denis
+    private JButton continuarButton;
+    private JButton sairButton;
+    private JLabel label1;
+    private JComboBox dropDown1;
+    // JFormDesigner - End of variables declaration  //GEN-END:variables  @formatter:on
 }
