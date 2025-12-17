ddd5e79b9b9466c00753333805192db00ff97847
TimeCa7alyst <denis_alves112@hotmail.com>
2025-12-17 13:45:38 -0300

    updated crud operations

--- src/main/java/DAO/AnalysisDAO.java
+++ src/main/java/DAO/AnalysisDAO.java
@@ -1,7 +1,6 @@
 package DAO;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import model.entities.Analysis;
@@ -27,27 +26,32 @@ public class AnalysisDAO {
         String sql = "INSERT INTO \"analysis\" (\"algorithm\", \"file\", \"date\", \"usr\", \"size\") " +
                 "VALUES (?, ?, ?, ?, ?)";
 
-        PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+        PreparedStatement statement = null;
 
-        PGobject pGobject = new PGobject();
-        pGobject.setType("jsonb");
-        pGobject.setValue(obj.getJsonFile().toString());
+        ResultSet rs = null;
 
-        LocalDateTime actualDate = (obj.getDate() != null) ? obj.getDate() : LocalDateTime.now();
+        try {
+            statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-        statement.setString(1, obj.getAlgorithm());
-        statement.setObject(2, pGobject);
-        statement.setTimestamp(3, Timestamp.valueOf(actualDate));
-        statement.setString(4, "ADMIN");
-        statement.setBigDecimal(5, obj.getSize());
+            PGobject pGobject = new PGobject();
+            pGobject.setType("jsonb");
+            pGobject.setValue(obj.getJsonFile().toString());
 
-        int rowsAffected = statement.executeUpdate();
+            LocalDateTime actualDate = (obj.getDate() != null) ? obj.getDate() : LocalDateTime.now();
 
-        if (rowsAffected <= 0) {
-            throw new RuntimeException("Failed to create object");
-        }
+            statement.setString(1, obj.getAlgorithm());
+            statement.setObject(2, pGobject);
+            statement.setTimestamp(3, Timestamp.valueOf(actualDate));
+            statement.setString(4, "ADMIN");
+            statement.setBigDecimal(5, obj.getSize());
 
-        try (ResultSet rs = statement.getGeneratedKeys()) {
+            int rowsAffected = statement.executeUpdate();
+
+            if (rowsAffected <= 0) {
+                throw new RuntimeException("Failed to create object");
+            }
+
+            rs = statement.getGeneratedKeys();
             if (rs.next()) {
                 Integer newId = rs.getInt(1);
 
@@ -60,19 +64,19 @@ public class AnalysisDAO {
                         obj.getSize())
                         .build();
             }
+            throw new SQLException("Failed to create Analysis");
+        } finally {
+            rs.close();
+            statement.close();
         }
-        throw new SQLException("Failed to create Analysis");
     }
 
     public List<Analysis> findAll() throws SQLException {
         List<Analysis> analyses = new ArrayList<Analysis>();
+        String sql = "SELECT * FROM \"analysis\"";
 
-        try {
-            String sql = "SELECT * FROM \"analysis\"";
-
-            PreparedStatement preparedStatement = connection.prepareStatement(sql);
-
-            ResultSet resultSet = preparedStatement.executeQuery();
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql);
+             ResultSet resultSet = preparedStatement.executeQuery();) {
 
             while (resultSet.next()) {
 
@@ -101,11 +105,62 @@ public class AnalysisDAO {
         return analyses;
     }
 
-    public List<Analysis> findById(Integer id) {
+    public Analysis findById(Integer id) throws SQLException, JsonProcessingException {
 
+        String sql = "SELECT * FROM \"analysis\" WHERE id = ?";
+
+        ResultSet resultSet = null;
+
+        PreparedStatement statement = null;
+
+        try {
+            statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, id);
+
+            resultSet = statement.executeQuery();
+
+            if (!resultSet.next())
+                throw new RuntimeException("Analysis not found");
+
+            String fileString = resultSet.getString("file");
+            JsonNode jsonNode = null;
+            if (fileString != null) {
+                jsonNode = mapper.readTree(fileString);
+            }
+            return new Analysis.Builder(
+                    resultSet.getInt("id"),
+                    resultSet.getString("algorithm"),
+                    jsonNode,
+                    resultSet.getTimestamp("date").toLocalDateTime(),
+                    resultSet.getString("usr"),
+                    resultSet.getBigDecimal("size"))
+                    .build();
+
+        } finally {
+            if (resultSet != null && statement != null) {
+                try {
+                    resultSet.close();
+                    statement.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 
     public void delete(Integer id) {
+        String sql = "DELETE FROM \"analysis\" WHERE id = ?";
 
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+
+            statement.setInt(1, id);
+
+            statement.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Error excluding analysis");
+            throw new RuntimeException(e);
+        }
     }
 }
